7baf2c4b2becf34e2ce028f5f863ab18c6b6be8c
Vineet Kumar <44500552+heisenberg-official@users.noreply.github.com>
2020-07-30 23:54:24 +0530

    Mailer Upgrade To TLS Using SendGrid

--- sendEmail.php
+++ sendEmail.php
@@ -1,33 +1,36 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    if (isset($_POST['name']) && isset($_POST['email'])) {
-        $name = $_POST['name'];
-        $email = $_POST['email'];
-        $subject = $_POST['subject'];
-        $body = $_POST['body'];
-        require_once "PHPMailer/PHPMailer.php";
-        require_once "PHPMailer/SMTP.php";
-        require_once "PHPMailer/Exception.php";
-        $mail = new PHPMailer();
-        $mail->isSMTP();
-        $mail->Host = "smtp.gmail.com";
-        $mail->SMTPAuth = true;
-        $mail->Username = "<YOUR@EMAIL.COM>";
-        $mail->Password = '<YOUR-PASSWORD>';
-        $mail->Port = 465;
-        $mail->SMTPSecure = "ssl";
-        $mail->isHTML(true);
-        $mail->setFrom($email, $name);
-        $mail->addAddress("tagclub.vitu@gmail.com");
-        $mail->Subject = $subject;
-        $mail->Body = '<h3 align=left>NAME: '.$_POST['name'].'<br><br>EMAIL: '.$_POST['email'].'<br><br>MESSAGE: '.$_POST['body'].'</h3>';
-        if ($mail->send()) {
-            $status = "success";
-            $response = "[--<> Email Sent Successfully <>--]";
-        } else {
-            $status = "failed";
-            $response = "Something is wrong: <br><br>" . $mail->ErrorInfo;
-        }
-        exit(json_encode(array("status" => $status, "response" => $response)));
+require 'vendor/autoload.php';
+
+if (isset($_POST['name']) && isset($_POST['email']))
+{
+    $MAIN_KEY="YOUR_SENDGRID_API_KEY";
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $body = $_POST['body'];
+
+    $email = new \SendGrid\Mail\Mail(); 
+    $email->setFrom("admin@tagclub.in", "TAG Official");
+    $email->setSubject($subject);
+    $email->addTo("ADMIN_RECIEVER@MAIL.COM", "Admin_Name");
+    $email->addContent("text/html", '<h3 align=left>NAME: '.$_POST['name'].'<br><br>EMAIL: '.$_POST['email'].'<br><br>MESSAGE: '.$_POST['body'].'</h3>');
+
+    $sendgrid = new \SendGrid($MAIN_KEY);
+    $response = $sendgrid->send($email);
+
+    $res=$response->statusCode();  // Unauth->404      Success->202
+
+    if($res == "202")
+    {
+        $status="success";
+        $response = "Email Sent Successfully";
     }
+    else
+    {
+        $status="failed";
+        $response = "Failed ".$res." Error Encountered.";
+    }
+    exit(json_encode(array("Status" => $status, "Response_Status_Code" => $res)));
+}
 ?>
